a3556a9ee9639bab61e1a367008412a27763025a
matheus rodrigues rosa <matheusrodriguesr@hotmail.com>
2021-08-11 19:20:03 -0300

    CRIACAO DO MENU

--- app/src/main/java/matheusrodrigues/androidapps/apprepositories/ui/MainActivity.kt
+++ app/src/main/java/matheusrodrigues/androidapps/apprepositories/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package matheusrodrigues.androidapps.apprepositories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import matheusrodrigues.androidapps.apprepositories.R
 import matheusrodrigues.androidapps.apprepositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -15,4 +17,20 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
